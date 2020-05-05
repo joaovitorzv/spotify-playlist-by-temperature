6c81c8ca2de4d899010cc34b1b05fe842d14a7fe
joao <resetgamer1@gmail.com>
2020-05-04 22:25:20 -0300

    Revert "finished"
    This reverts commit 0fbdcb48bc9c88d2967f50cb92dec491c41bf30a.

--- src/services/spotify.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const spotify =  axios.create({
-  baseURL: 'https://api.spotify.com/v1'
-});
-
-module.exports = spotify;
--- src/services/weather.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const api =  axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5'
-});
-
-module.exports = api;
